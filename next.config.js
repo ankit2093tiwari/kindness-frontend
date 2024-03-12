ddd5ff4761ce3c693e3e325f6beadcf9cfab7faf
ankit2093tiwari <39435198+ankit2093tiwari@users.noreply.github.com>
2024-03-12 15:33:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,16 +7,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // env: {
-  //   NEXT_APP_ENV: process.env.NEXT_APP_ENV,
-  //   SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
-  //   API_URL: process.env.NEXT_PUBLIC_API_URL,
-  //   BASEURL: process.env.BASEURL,
-  //   BASE_LIVE_URL: process.env.NEXT_PUBLIC_BASE_LIVE_URL,
-  //   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-  //     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-  //   STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  // },
+  env: {
+     NEXT_PUBLIC_NEXT_APP_ENV: process.env.NEXT_PUBLIC_NEXT_APP_ENV,
+     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+     NEXT_PUBLIC_BASEURL: process.env.NEXT_PUBLIC_BASEURL,
+     NEXT_PUBLIC_BASE_LIVE_URL: process.env.NEXT_PUBLIC_BASE_LIVE_URL,
+     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+     NEXT_PUBLIC_STRIPE_SECRET_KEY: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
+  },
 };
 
 module.exports = nextConfig;
